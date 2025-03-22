00ddf027b30031ee9249193679bebfb7d0a67ba8
crusadevs <jacobsworkmail08@gmail.com>
2025-03-22 12:38:44 +0200

    db setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const PORT = 8080 || process.env.PORT;
+
+//Middleware
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
+app.use(express.json({ limit: '50mb' }))
+
+//MongoDB
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB Atlas: ', error);
+    });
+
